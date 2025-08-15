98986e4f1af668b366c42b69550741f9f98db535
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-08-15 16:29:58 +0300

    ass either class

--- lib/Features/home/presentation/views/domin/repos/home_repo.dart
+++ lib/Features/home/presentation/views/domin/repos/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:bokly_app/Features/home/presentation/views/domin/entittes/book_entity.dart';
+import 'package:bokly_app/core/errors/filer.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo { 
-  Future<List<BookEntity>> fetchFutcherBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFutcherBooks();
   Future<List<BookEntity>> fetchNewsBooks();
 }
