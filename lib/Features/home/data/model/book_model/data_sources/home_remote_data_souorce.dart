256ee967b939b6fb24ecf6fddd15fd8bda4fe0f0
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-08-17 10:45:01 +0300

    implement remote fetch dutcherd books

--- lib/Features/home/data/model/book_model/data_sources/home_remote_data_souorce.dart
+++ lib/Features/home/data/model/book_model/data_sources/home_remote_data_souorce.dart
@@ -1,4 +1,6 @@
+import 'package:bokly_app/Features/home/data/model/book_model/book_model.dart';
 import 'package:bokly_app/Features/home/presentation/views/domin/entittes/book_entity.dart';
+import 'package:bokly_app/core/utils/api_serves.dart';
 
 abstract class HomeRemoteDataSouorce {
    Future<List<BookEntity>> etchFutcherBooks(); 
@@ -6,10 +8,23 @@ abstract class HomeRemoteDataSouorce {
 } 
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSouorce{
-  @override
-  Future<List<BookEntity>> etchFutcherBooks() {
-    // TODO: implement etchFutcherBooks
-    throw UnimplementedError();
+   final ApiServes apiServes ;
+
+  HomeRemoteDataSourceImpl( this.apiServes);
+  Future<List<BookEntity>> etchFutcherBooks() async {
+    var data = await apiServes.get(endBiond: 'volumes?filtering=free-ebooks&q=programming') ;
+
+    List<BookEntity> books = getBooksList(data); 
+    return books ;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [] ; 
+    
+    for(var bookMap in data['items']){
+      books.add(BookModel.fromJson(bookMap)) ;
+    }
+    return books;
   }
 
   @override
