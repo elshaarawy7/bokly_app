df983d1ac83a43e4e0123ca16f2c2a75db304544
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-08-18 10:27:23 +0300

    implement cupit fetch futcherd box

--- lib/Features/home/presentation/manger/cubit/futcherd_books_cubit.dart
+++ lib/Features/home/presentation/manger/cubit/futcherd_books_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bokly_app/Features/home/presentation/views/domin/entittes/book_entity.dart';
+import 'package:bokly_app/Features/home/presentation/views/domin/usecase/fetch_futcher_books_usecase.dart';
+import 'package:bokly_app/core/errors/filer.dart';
 import 'package:meta/meta.dart';
 
 part 'futcherd_books_state.dart';
 
-class FutcherdBooksCubit extends Cubit<FutcherdBooksState> {
-  FutcherdBooksCubit() : super(FutcherdBooksInitial());
+class FutcherdBooksCubit extends Cubit<FutcherdBooksState>  {
+  FutcherdBooksCubit(this.fetchFutcherBooksUsecase) : super(FutcherdBooksInitial()); 
+
+  final FetchFutcherBooksUsecase fetchFutcherBooksUsecase ; 
+  Future<void>fetchFutcherdBox() async{
+    emit(FutcheredBooksLoding()) ; 
+  var result = await fetchFutcherBooksUsecase.call() ;
+  result.fold(
+    (failure) => emit(FutcherBooksFuiler(failure.message)),
+    (books) => emit(FutcherdBoksSacsess(books)),
+  );
+  }
+
 }
